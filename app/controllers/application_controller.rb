e885f633fbbf0934198ac38de778a1fe5cd11b3f
Christian Doucette <cdoucette826@gmail.com>
2020-09-13 19:22:46 -0400

    Search feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,17 @@ class ApplicationController < ActionController::Base
   require 'rspotify'
 
   def homepage
-    #playlist = RSpotify::Playlist.find('usernameistakenistaken', '4GFyJzIlynP7mjBNnRe1sA')
-    #puts(playlist.name)
-    song_id = RSpotify::Track.search('Howlin').first.id
-    #song_id = '1SDiiE3v2z89VxC3aVRKHQ'
-    @song = RSpotify::Track.find(song_id)
-    @audio_features = RSpotify::AudioFeatures.find(song_id)
     render({ :template => "general/homepage.html.erb" })
+  end
+
+  def homepage_with_query
+    song_search = params.fetch(:song_from_query)
+    @song = RSpotify::Track.search(song_search).first
+    if @song.nil?
+      redirect_to("/", { :alert => "No song returned" })
+    else
+    @audio_features = RSpotify::AudioFeatures.find(@song.id)
+    render({ :template => "general/homepage.html.erb" })
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  #Route for home page
   match("/", { :controller => "application", :action => "homepage", :via => "get"})
+
+  #Home page after searching for a song
+  match("/song_search", { :controller => "application", :action => "homepage_with_query", :via => "get"})
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
