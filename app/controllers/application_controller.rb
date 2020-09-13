70f5daf56ca0d2c19850431ff40c61942244f101
Christian Doucette <cdoucette826@gmail.com>
2020-09-13 16:41:21 -0400

    Home page now displays data for a specific song

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
   require 'rspotify'
 
   def homepage
-    playlist = RSpotify::Playlist.find('usernameistakenistaken', '4GFyJzIlynP7mjBNnRe1sA')
-
-    puts(playlist.tracks_added_at)
-    @am_popularity = RSpotify::Artist.search('Arctic Monkeys').first.popularity
+    #playlist = RSpotify::Playlist.find('usernameistakenistaken', '4GFyJzIlynP7mjBNnRe1sA')
+    #puts(playlist.name)
+    #@song = RSpotify::Track.search('Tighten Up').first
+    @song = RSpotify::Track.find('2MVwrvjmcdt4MsYYLCYMt8')
+    @audio_features = RSpotify::AudioFeatures.find('2MVwrvjmcdt4MsYYLCYMt8')
     render({ :template => "general/homepage.html.erb" })
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def formatKey(keyNum, modality)
+    keyArray = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    if modality
+      modalityStr = "minor"
+    else
+      modalityStr = "major"
+    end
+    return "#{keyArray[keyNum]} #{modalityStr}"
+  end
 end
