4cd6d3845f5cfc027886fab5adb6509f528427d7
Christian Doucette <cdoucette826@gmail.com>
2020-09-29 00:29:48 -0500

    User top tracks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def spotify_callback
     @user = RSpotify::User.new(request.env['omniauth.auth'])
-    @tracks = @user.saved_tracks(limit: 50)
+    @top_tracks = @user.top_tracks(limit: 50, offset: 0, time_range: 'long_term')
 
     #top_songs = spotify_user.top_tracks(limit: 20, offset: 0, time_range: 'short_term').size #=> (Track array)
 
