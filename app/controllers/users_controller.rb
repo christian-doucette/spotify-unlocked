1058648fb54c243cabf042ac052868e309c80a29
jmdoucette <jmdoucette41@gmail.com>
2020-10-04 01:05:10 -0500

    layouts, some user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,12 @@
 class UsersController < ApplicationController
   require 'rspotify'
-  #isnt working right now, but will never be run from home
-  def user_data
-    user = RSpotify::Artist.find("jmdoucette-no")
-    render({:template => "users/user_data.html.erb"})
-  end
 
 
   def spotify_callback
     @user = RSpotify::User.new(request.env['omniauth.auth'])
-    @top_tracks = @user.top_tracks(limit: 50, offset: 0, time_range: 'long_term')
+    @top_tracks = @user.top_tracks(limit: 20, offset: 0, time_range: 'long_term')
+    @top_artists = @user.top_artists(limit:20, offset:0,time_range: 'long_term')
+    @playlists = @user.playlists(limit:20, offset:0)
 
     #top_songs = spotify_user.top_tracks(limit: 20, offset: 0, time_range: 'short_term').size #=> (Track array)
 
