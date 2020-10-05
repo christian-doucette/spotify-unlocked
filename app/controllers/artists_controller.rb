17648d108abae702f413c577181c35987b54f017
Christian Doucette <cdoucette826@gmail.com>
2020-10-05 11:56:43 -0500

    Extended top songs

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -23,7 +23,15 @@ class ArtistsController < ApplicationController
   def artist_data_with_display
     #Displays an artist and search bar on artist data page, given artist_id
     artist_id = params.fetch(:artist_id)
+    puts("artist id: ")
+    puts(artist_id)
+    puts("")
     @artist = RSpotify::Artist.find(artist_id)
+    @extended_top_tracks = RSpotify::Track.search(@artist.name, limit: 50).sort_by {|track| 100-track.popularity}.filter {|track|
+      puts(track.artists.first.id)
+      puts(track.artists.first.id == @artist.id)
+      track.artists.first.id == @artist.id}
+
     render({ :template => "artists/artist_data.html.erb" })
   end
 
