459734981a18f06481c2551a360f2e90a9fd7388
Christian Doucette <cdoucette826@gmail.com>
2020-10-06 13:34:00 -0500

    Added get_all_tracks function to artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -24,19 +24,41 @@ class ArtistsController < ApplicationController
     #Displays an artist and search bar on artist data page, given artist_id
     artist_id = params.fetch(:artist_id)
     @artist = RSpotify::Artist.find(artist_id)
-    @all_top_tracks = RSpotify::Track.search(@artist.name, limit: 50).sort_by {|track| 100-track.popularity}.filter {|track| track.artists.first.id == @artist.id}
 
-    #This thing gets all of an artists songs by getting all albums then all songs from those albums.
-    #Its cool but makes too many API requests. May be able to improve it later
-
-    #@all_top_tracks = Array.new
-    #@artist.albums(limit: 50, album_type: 'album,single').each do |album|
-    #  @all_top_tracks.concat(album.tracks)
-    #end
-    #@all_top_tracks = all_tracks.sort_by {|track| 100-track.popularity}
+    #@all_top_tracks = RSpotify::Track.search(@artist.name, limit: 50).sort_by {|track| 100-track.popularity}.filter {|track| track.artists.first.id == @artist.id}
+    @all_tracks = get_all_tracks(@artist)
+    @all_tracks.sort_by! {|track| 100-track.popularity}
 
 
     render({ :template => "artists/artist_data.html.erb" })
+  end
+
+
+
+
+  #----------------------------------------------------------------------------#
+  #-------Business logic functions that would usually go in a model------------#
+  #----------------------------------------------------------------------------#
+
+
+  def get_all_tracks(artist)
+    #This functions gets all the US artists
+    all_tracks_ids = %w() #empty string array
+    artist.albums(limit: 50, album_type: 'album,single', market: 'US').each do |album|
+      album.tracks.each do |track|
+        all_tracks_ids.append(track.id.to_s)
+      end
+    end
+
+    all_tracks = Array.new
+    i = 0
+    while i < all_tracks_ids.length()
+      next_fifty_tracks = RSpotify::Track.find(all_tracks_ids[i, 50])
+      all_tracks.concat(next_fifty_tracks)
+      i += 50
+    end
+
+    return all_tracks
   end
 
 end
