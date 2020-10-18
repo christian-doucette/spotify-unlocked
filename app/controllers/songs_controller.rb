1546395e6fc2d5369be0867242c708f11890efbb
Christian Doucette <cdoucette826@gmail.com>
2020-10-18 12:37:34 -0500

    Cleaned up chord fit function

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -36,6 +36,8 @@ class SongsController < ApplicationController
   def chords_page
     song_id = params.fetch(:song_id)
     @song = RSpotify::Track.find(song_id)
+    @af = RSpotify::AudioFeatures.find(song_id)
+    puts(@af.tempo)
 
     url = "audio-analysis/#{song_id}"
     audio_analysis = RSpotify.get(url)
@@ -47,7 +49,7 @@ class SongsController < ApplicationController
     #audio_analysis["bars"].first(20).each do |bar|
     #  puts("Bar Start: #{bar["start"]}, duration: #{bar["duration"]}, end: #{bar["start"]+bar["duration"]}")
     #end
-    @chords_per_beat = get_chords_per_beat(audio_analysis)
+    @chords_per_bar = get_chords_per_bar(audio_analysis)
     @chords = get_chords(audio_analysis)
     render({ :template => "songs/chords.html.erb" })
   end
@@ -71,6 +73,7 @@ class SongsController < ApplicationController
   end
 
 
+
   def dot(x, y) #larger means closer
     sum = 0
     i = 0
@@ -82,19 +85,24 @@ class SongsController < ApplicationController
   end
 
 
-  def div(vector, divisor)
-    ret_array = Array.new
-    for entry in vector
-      ret_array.append(entry/divisor)
+
+  def cosine_similarity(u, v)
+    u_mag = 0
+    v_mag = 0
+    u_dot_v = 0
+
+    for i in 0..(u.length()-1)
+      u_mag += u[i]**2
+      v_mag += v[i]**2
+      u_dot_v += u[i] * v[i]
     end
-    return ret_array
+
+    u_mag = Math.sqrt(u_mag)
+    v_mag = Math.sqrt(v_mag)
+    return u_dot_v / (u_mag * v_mag)
   end
 
 
-  def cosine_distance(x, y) #larger means closer
-    zero_vec = [0,0,0,0,0,0,0,0,0,0,0,0]
-    return dot(x, y)/ (euclidean_distance(x, zero_vec) * dot(y, zero_vec))
-  end
 
 
   def format_key(keyNum, modality)
@@ -109,7 +117,7 @@ class SongsController < ApplicationController
     chords = Array.new
     audio_analysis["segments"].each do |segment|
       if segment["confidence"] > 0.5 #0.7 is a strict one
-        new_chord = get_chord_from_pitch_vec_dot(segment["pitches"])
+        new_chord = chord_fit_euclidean(segment["pitches"])
         chords.append(new_chord)
       end
     end
@@ -119,55 +127,55 @@ class SongsController < ApplicationController
 
 
 
-  def get_chords_per_beat(audio_analysis)
+  def get_chords_per_bar(audio_analysis)
     segments = audio_analysis["segments"]
-    beats = audio_analysis["bars"]
-    first_beat = {"start" => 0.0, "duration" => beats[0]["start"], "confidence" => 1}
-    puts("added first beat")
-    puts(first_beat["confidence"])
+    bars = audio_analysis["bars"]
+    first_bar = {"start" => 0.0, "duration" => bars[0]["start"], "confidence" => 1.0}
+    bars.unshift(first_bar)
 
-    beats.unshift(first_beat)
-    #puts(segments.length())
+
     chords = Array.new
-    beat_index = 0
+    bar_index = 0
     seg_index = 0
-    beats.first(20).each do |beat|
-      beat_start = beat["start"]
-      beat_end = beat_start + beat["duration"]
-      puts("Start: #{beat_start}, duration: #{beat["duration"]}, end: #{beat_end}")
-      #puts(beat["duration"])
-      if beat["confidence"] > 0.0 #0.7 is a strict one
+    bars.first(20).each do |bar|
+      bar_start = bar["start"]
+      bar_end = bar_start + bar["duration"]
+      puts("Start: #{bar_start}, duration: #{bar["duration"]}, end: #{bar_end}")
+      if bar["confidence"] > 0.0 #0.7 is a strict one
 
-        while segments[seg_index]["start"] + segments[seg_index]["duration"] < beat_start
+        #Skips the segments before this one
+        while segments[seg_index]["start"] + segments[seg_index]["duration"] < bar_start
           seg_index += 1
         end
 
-        while segments[seg_index]["start"] < beat_end
+
+        #For each segment included in this beat, creates a weighted sum of pitch vectors
+        while segments[seg_index]["start"] < bar_end
           seg = segments[seg_index]
           seg_start = seg["start"]
           seg_end = seg_start + seg["duration"]
           chord_total = [0,0,0,0,0,0,0,0,0,0,0,0]
 
 
-          if seg_start < beat_start
-            duration = seg_end - beat_start
-          elsif beat_end < seg_end
-            duration = beat_end - seg_start
+          if seg_start < bar_start
+            duration = seg_end - bar_start
+          elsif bar_end < seg_end
+            duration = bar_end - seg_start
           else
             duration = seg_end - seg_start
           end
 
           for k in 0..11
-            chord_total[k] += ((seg["pitches"][k] * duration) / beat["duration"] )
+            chord_total[k] += ((seg["pitches"][k] * duration) / bar["duration"] )
           end
 
           seg_index += 1
         end
-        combined_chord = get_chord_from_pitch_vec(chord_total)
-        chords.append("Beat: #{beat_index}, chord: #{combined_chord}")
+        combined_chord = chord_fit_cosine(chord_total)
+        chords.append("Bar: #{bar_index}, chord: #{combined_chord}")
 
         seg_index -= 1
-        beat_index += 1
+        bar_index += 1
       end
     end
 
@@ -176,7 +184,7 @@ class SongsController < ApplicationController
 
 
 
-  def get_chord_from_pitch_vec(pitch_vec)
+  def chord_fit_euclidean(pitch_vec)
     bestFitPitch = -1
     bestFitModality = -1
     bestFitVal = 4 #higher than max possible distance
@@ -197,19 +205,19 @@ class SongsController < ApplicationController
 
 
 
-  def get_chord_from_pitch_vec_dot(pitch_vec)
+  def chord_fit_cosine(pitch_vec)
     bestFitPitch = -1
     bestFitModality = -1
-    bestFitVal = 0 #higher than max possible distance
-    chords = [[1,0,0,0,1,0,0,1,0,0,0,0],[1,0,0,1,0,0,0,1,0,0,0,0],[1,0,0,0,0,1,0,1,0,0,0,0],[1,0,1,0,0,0,0,1,0,0,0,0],[1,0,0,1,0,0,1,0,0,0,0,0],[1,0,0,0,1,0,0,0,1,0,0,0]]
+    bestFitVal = 0
+    chord_templates = [[1,0,0,0,1,0,0,1,0,0,0,0],[1,0,0,1,0,0,0,1,0,0,0,0]]
 
     for i in 0..11
       for j in 0..1 #0..chords.length-1 #put that instead for more chord varieties
-        distance = cosine_distance(chords[j].rotate(-i), pitch_vec)
-        if distance > bestFitVal
+        similarity = cosine_similarity(chord_templates[j].rotate(-i), pitch_vec)
+        if similarity > bestFitVal
           bestFitPitch = i
           bestFitModality = j
-          bestFitVal = distance
+          bestFitVal = similarity
         end
       end
     end
